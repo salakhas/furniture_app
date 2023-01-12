87561e984f2e2c34cf0c9c5c7e65a2a2afff15a8
Salakha S <87031922+salakhas@users.noreply.github.com>
2023-01-12 10:44:49 +0530

    updated furniture model

--- lib/model/furniture_model.dart
+++ lib/model/furniture_model.dart
@@ -3,12 +3,20 @@ class Furniture {
   String name;
   double rating;
   double price;
+  String description;
+  String line1;
+  String line2;
+  String line3;
 
   Furniture({
     required this.imageUrl,
     required this.name,
     required this.rating,
     required this.price,
+    required this.description,
+    required this.line1,
+    required this.line2,
+    required this.line3,
   });
 }
 
@@ -18,17 +26,29 @@ final List<Furniture> furnitures = [
     name: 'Chair',
     rating: 4.8,
     price: 47.35,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+    line1: "Maximum weight: up to 40kg",
+    line2: "Morbi commodo hendrerit magna vel maximus.",
+    line3: "Donec nec eros massa.",
   ),
   Furniture(
     imageUrl: 'assets/images/gardenFurniture.png',
     name: 'Garden Furniture',
     rating: 4.0,
     price: 99.0,
+    description: "Cras imperdiet turpis quis pretium tempor.",
+    line1: "Maximum weight: up to 40kg",
+    line2: "Morbi commodo hendrerit magna vel maximus.",
+    line3: "Donec nec eros massa.",
   ),
   Furniture(
     imageUrl: 'assets/images/mirror.png',
     name: 'Golden Leaf Mirror',
     rating: 4.6,
     price: 150,
+    description: "Maecenas gravida vestibulum elit quis fringilla. ",
+    line1: "Maximum weight: up to 40kg",
+    line2: "Morbi commodo hendrerit magna vel maximus.",
+    line3: "Donec nec eros massa.",
   ),
 ];
